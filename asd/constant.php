c4844bf2b84098530a8b2a21834801fa1be5e2cc
Bunnyburning <LeoPeyretti@Outlook.com>
2018-07-20 15:21:49 -0300

    Add files via upload

---
+++ asd/constant.php
@@ -0,0 +1,5 @@
+<?php
+//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
+define('SITE_KEY',"6Lc4LGUUAAAAABLhfemx1O7p7u6AmWd2Lgr4jJnM"); 
+define('SECRET_KEY',"6Lc4LGUUAAAAAIsQeXpHzGuPD0Wz5x17P2sdriKY")
+?>
